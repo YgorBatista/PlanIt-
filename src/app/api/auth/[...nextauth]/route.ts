96ae43aa23e232e44d9f7d10713ff4f982015db8
Ygor <ygorbatista14@gmail.com>
2025-11-02 03:53:15 -0300

    correcao no route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,19 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import GitHubProvider  from 'next-auth/providers/github'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
 
 const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID!,
-            clientSecret: process.env.GOOGLE_SECRET!
+            clientSecret: process.env.GOOGLE_SECRET!,
         }),
         GitHubProvider({
             clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!
-        })
-
-        
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
-})
+});
 
-export default ( req:any, res:any) => {
-
-if (process.env.VERCEL_URL){
-    process.env.NEXTAUTH_URL = `https://${process.env.VERCEL_URL}`
-
-}
-return NextAuth(req, res, handler)
-
-}
+export { handler as GET, handler as POST };
